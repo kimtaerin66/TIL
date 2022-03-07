e447e5b772fce2e2b86e3e263b8f5403cb3a0ccd
kimtaerin66 <yeonjikim66@gmail.com>
2022-03-07 22:34:31 +0900

    styled-components 공부중

---
+++ Clone_coding/movie_master/src/App.js
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+const Father = styled.div`
+display : flex;
+`;
+
+const Box = styled.div`
+background-color :${props =>props.bgColor};
+width: 100px;
+height : 100px;
+`;
+
+
+function App() {
+  return (
+  
+  <Father>
+   <Box bgColor="tomato"/>
+   <Box bgColor="teal"/>
+  </Father>
+   );
+
+}
+
+export default App;
