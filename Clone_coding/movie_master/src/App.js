3ebf1d07e675ecd2fb7550b2f984e69585cf69a2
kimtaerin66 <yeonjikim66@gmail.com>
2022-03-08 17:47:45 +0900

    작성중

--- Clone_coding/movie_master/src/App.js
+++ Clone_coding/movie_master/src/App.js
@@ -1,23 +1,46 @@
-import styled from "styled-components";
+import styled,{ keyframes } from "styled-components";
 
-const Father = styled.div`
+const Wrapper = styled.div`
 display : flex;
 `;
 
+const Emoji = styled.span`
+  font-size: 36px;
+`;
+const rotationAnimation = keyframes`
+ 0% {
+   transform: rotate(0deg);
+   border-radius: 0px;
+ }
+ 50% {
+  border-radius: 100px;
+ }
+ 100%{
+  transform: rotate(360deg);
+  border-radius: 0px;
+ }
+`;
+
 const Box = styled.div`
-background-color :${props =>props.bgColor};
-width: 100px;
-height : 100px;
+  width:200px ;
+  height: 200px;
+  background-color: tomato;
+  display: flex;
+  justify-content: center;
+  align-items:center;
+  animation: ${rotationAnimation} 1s linear infinite;
+  ${Emoji}:hover{
+      font-size: 98px;
+
+    }
 `;
 
 
 function App() {
   return (
   
-  <Father>
-   <Box bgColor="tomato"/>
-   <Box bgColor="teal"/>
-  </Father>
+  <Wrapper>
+  </Wrapper>
    );
 
 }
