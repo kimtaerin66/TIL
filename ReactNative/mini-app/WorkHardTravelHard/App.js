904604e4fa6a533476fb1f0019e0b3080c2b7f90
kimtaerin66 <yeonjikim66@gmail.com>
2022-05-13 17:50:43 +0900

    완료

--- ReactNative/mini-app/WorkHardTravelHard/App.js
+++ ReactNative/mini-app/WorkHardTravelHard/App.js
@@ -1,86 +1,129 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, ScrollView,Text, View,TouchableOpacity,TextInput, Alert } from "react-native";
+import {
+  Platform,
+  StyleSheet,
+  ScrollView,
+  Text,
+  View,
+  TouchableOpacity,
+  TextInput,
+  Alert,
+} from "react-native";
 import { theme } from "./colors";
-import { useState } from 'react';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { useEffect } from 'react';
-import { FontAwesome } from '@expo/vector-icons'; 
+import { useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useEffect } from "react";
+import { FontAwesome } from "@expo/vector-icons";
 
 export default function App() {
-  const [ working, setWorking ] = useState(true);
-  const [ text, setText ] = useState("");
+  const [working, setWorking] = useState(true);
+  const [text, setText] = useState("");
   //보통 기본값으로 빈배열을 쓰지만, 오브젝트 사용
-  const [ toDos, setToDos ] = useState({});
-   //앱이 실행될때 바로실행
-   useEffect(()=>{
+  const [toDos, setToDos] = useState({});
+  //앱이 실행될때 바로실행
+  useEffect(() => {
     loadToDos();
-  },[]);
+  }, []);
   const travel = () => setWorking(false);
   const work = () => setWorking(true);
   const onChangeText = (e) => setText(e);
-  const saveToDos = async(toSave) => {
+  const saveToDos = async (toSave) => {
     //현재가지고있는 todo를 string으로만들고 저장
-   await AsyncStorage.setItem("@toDos", JSON.stringify(toSave));
+    await AsyncStorage.setItem("@toDos", JSON.stringify(toSave));
   };
-  const loadToDos = async() => {
-   const s = await AsyncStorage.getItem("@toDos");
-   setToDos(JSON.parse(s));//다시 obj로 만들기
-  }
-  const addToDo = async() => {
-    if(text === ""){//작성된게없다면
+  const loadToDos = async () => {
+    const s = await AsyncStorage.getItem("@toDos");
+    if (s) {
+      setToDos(JSON.parse(s)); //다시 obj로 만들기
+    }
+  };
+  const addToDo = async () => {
+    if (text === "") {
+      //작성된게없다면
       return;
     }
     const newToDos = {
-         ...toDos,
-        [Date.now()]: { text , working },
-  };
-  setToDos(newToDos);
-  await saveToDos(newToDos);
-  setText("");
-};
-const deleteToDo = async(key) => {
-  Alert.alert("Delete To Do",
-   "Are you sure?",[
-   {text : "Cancle" },
-   {text : "I'm Sure",
-   style:"destructive",
-   onPress: () => {
-    const newToDos = {...toDos}//복사
-    delete newToDos[key]
+      ...toDos,
+      [Date.now()]: { text, working },
+    };
     setToDos(newToDos);
-   saveToDos(newToDos);
-   }},
-   ]);
-};
+    await saveToDos(newToDos);
+    setText("");
+  };
+  const deleteToDo = async (key) => {
+    if (Platform.OS === "web") {
+      const ok = confirm("Do you want to delete this To Do?");
+      if (ok) {
+        const newToDos = { ...toDos };
+        delete newToDos[key];
+        setToDos(newToDos);
+        saveToDos(newToDos);
+      }
+    } else {
+      Alert.alert("Delete To Do", "Are you sure?", [
+        { text: "Cancle" },
+        {
+          text: "I'm Sure",
+          style: "destructive",
+          onPress: () => {
+            const newToDos = { ...toDos }; //복사
+            delete newToDos[key];
+            setToDos(newToDos);
+            saveToDos(newToDos);
+          },
+        },
+      ]);
+    }
+  };
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <View style={styles.header}>
         <TouchableOpacity onPress={work}>
-        <Text style={{...styles.btnText, color : working ? "white" : theme.gray}}>Work</Text>
+          <Text
+            style={{
+              fontSize: 32,
+              fontWeight: "600",
+              color: working ? "white" : theme.gray,
+            }}
+          >
+            Work
+          </Text>
         </TouchableOpacity>
         <TouchableOpacity onPress={travel}>
-        <Text style={{...styles.btnText, color: working ? theme.gray : "white"}}>Travel</Text>
+          <Text
+            style={{
+              fontSize: 32,
+              fontWeight: "600",
+              color: working ? theme.gray : "white",
+            }}
+          >
+            Travel
+          </Text>
         </TouchableOpacity>
       </View>
-        <TextInput
+      <TextInput
         onSubmitEditing={addToDo}
-        value={text} 
+        value={text}
         returnKeyType="done"
         onChangeText={onChangeText}
-        placeholder={working ? "Add a To Do" : "Where do you wanna go?"} style={styles.input} />
-        <ScrollView>
-        {Object.keys(toDos).map(key => 
-        toDos[key].working === working ? (
-        <View style={styles.toDo} key={key}>
-            <Text style={styles.toDoText}>{toDos[key].text}</Text>
-         <TouchableOpacity onPress={()=> deleteToDo(key)}>
-           <Text><FontAwesome name="close" size={20} color={theme.gray} /></Text>
-           </TouchableOpacity>
-          </View>
+        placeholder={working ? "Add a To Do" : "Where do you wanna go?"}
+        style={styles.input}
+      />
+      <ScrollView>
+        {Object.keys(toDos).map((key) =>
+          toDos[key].working === working ? (
+            <View style={styles.toDo} key={key}>
+              <Text style={styles.toDoText}>{toDos[key].text}</Text>
+              <TouchableOpacity onPress={() => deleteToDo(key)}>
+                <Text>
+                  <FontAwesome name="close" size={20} color={theme.gray} />
+                </Text>
+              </TouchableOpacity>
+            </View>
           ) : null
         )}
-        </ScrollView>
+      </ScrollView>
     </View>
   );
 }
@@ -88,7 +131,7 @@ const deleteToDo = async(key) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor:theme.bg,
+    backgroundColor: theme.bg,
     //일반css에는 없는 padding속성
     paddingHorizontal: 20,
   },
@@ -97,32 +140,28 @@ const styles = StyleSheet.create({
     justifyContent: "space-between",
     flexDirection: "row",
   },
-  btnText: {
-    fontSize: 32,
-    fontWeight: "600",
-  },
   input: {
     backgroundColor: "white",
     paddingVertical: 15,
-    paddingHorizontal : 20,
-    borderRadius:30,
+    paddingHorizontal: 20,
+    borderRadius: 30,
     marginTop: 20,
     fontSize: 18,
-    marginVertical : 20,
+    marginVertical: 20,
   },
   toDo: {
-    backgroundColor : theme.toDoBg,
-    marginBottom : 10,
-    paddingVertical : 20,
-    paddingHorizontal : 40,
+    backgroundColor: theme.toDoBg,
+    marginBottom: 10,
+    paddingVertical: 20,
+    paddingHorizontal: 40,
     borderRadius: 15,
-    flexDirection:"row",
+    flexDirection: "row",
     alignItems: "center",
     justifyContent: "space-between",
-   },
-  toDoText : {
-   color:  "white",
-   fontSize : 17,
-   fontWeight : "500",
   },
-})
+  toDoText: {
+    color: "white",
+    fontSize: 17,
+    fontWeight: "500",
+  },
+});
