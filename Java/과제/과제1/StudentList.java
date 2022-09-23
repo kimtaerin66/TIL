930eb56ebd364680fb88b3a03cfb5ed36f15dd1c
Taerin <85108615+kimtaerin66@users.noreply.github.com>
2022-09-23 17:09:56 +0900

    Update StudentList.java

--- Java/과제/과제1/StudentList.java
+++ Java/과제/과제1/StudentList.java
@@ -1,4 +1,4 @@
-package study1;
+package study1_1;
 
 import java.io.*;
 import java.util.ArrayList;
@@ -9,44 +9,48 @@ import java.util.Comparator;
 import javax.swing.JOptionPane;
 
 public class StudentList {
-
 	
-	public static void main(String[] args) throws IOException {
+	//초기 학생수 랜덤 (1~20)
+	public  int count;
 	
+	  public  StudentList() {
+		  this.count = randomCount();
+	  }
+	  public int randomCount() {
+  	  return (int)(Math.random() *20)+1;
+   }
+	 
 		
-		//학생들의 성적표가 담긴 list
+	public static void main(String[] args) throws IOException {
          ArrayList<Student> list = new ArrayList<Student>();
+         StudentList s = new StudentList();
 
-		//초기 학생수 입력 필요
-		String inputCount  = JOptionPane.showInputDialog("학생 수를 입력해주세요.");
-		int  getCount= Integer.parseInt(inputCount);
-
-   	   for(int i=0; i < getCount; i++) {
-   		   list.add(new Student());
-       }
-   	   
+         //받아온 랜덤학생수만큼 Student 객체 생성하여 list에 추가
+         for(int i=0; i < s.randomCount(); i++) {
+        	 list.add(new Student());
+         }
+  
  	  //평균값으로 정렬 
    	Comparator<Student> comparator = new Comparator<Student>() {
    		@Override
    		public int compare(Student o1, Student o2) {
-   				return o2.getAverage()  - o1.getAverage();
+   			return o2.getAverage()  - o1.getAverage();
    		}	
    	};
-  	System.out.println("정렬 전" + list); //정렬 전
+ 	//System.out.println("정렬 전" + list); //정렬 전
    	Collections.sort(list, comparator);
-   	System.out.println("정렬 후" + list); //정렬 후
-
+   //	System.out.println("정렬 후" + list); //정렬 후
    	
    	//csv파일로 저장하기
     // 생성할 파일의 경로 및 파일명 으로 File 객체 생성
     File file = new File("D:/study/Java/학생성적표.csv");
     FileWriter fw = new FileWriter(file);
     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file), "MS949"));
-    bw.write("이름,국어,수학,영어,국사,평균");
+   bw.write("이름,국어,수학,영어,국사,평균");
     bw.newLine();
-    
+//    
    for(int i=0; i<list.size();i++) {
-	   bw.write(list.get(i)+",");
+   bw.write(list.get(i)+",");
        bw.newLine();
    }
    bw.close();
