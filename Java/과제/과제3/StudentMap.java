d1189f61e1c876533083924f2fd9fd698c77909a
Taerin <85108615+kimtaerin66@users.noreply.github.com>
2022-10-07 15:58:49 +0900

    Update StudentMap.java

--- Java/과제/과제3/StudentMap.java
+++ Java/과제/과제3/StudentMap.java
@@ -1,5 +1,6 @@
 package study2;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -10,74 +11,49 @@ import java.util.Scanner;
 
 public class StudentMap {
 
-	private String lastName;
 
-	public void getKey() {
-		// 사용자 입력받기
-		Scanner sc = new Scanner(System.in);
-		System.out.println("이름 또는 성을 입력하세요");
-		this.lastName = sc.next();
-	}
-
-	public StudentMap() {
-	}
-
+	public StudentMap() {}
 	
-	public static void main_ex(String[] args) {
-		
+	public static void main(String[] args) throws IOException {
 		CSVreader csvReader 		= new CSVreader();
-		List<Student> 	stuList 	= csvReader.readCSV_ex("../././.csv");
 		
+		//읽어온 파일을 stuList에 담기
+		List<Student> 	stuList 	= csvReader.readCSV("D:/study/Java/학생성적표.csv");
+	
+		//map생성. key=이름 또는 성, value= Student.
 		Map<String, Student> stdMap = new HashMap<String, Student> ();
 		
-		for( Student item : stuList) {
-			stdMap.put(item.getName(), item);
-		}
+       for( Student item : stuList) {
+		stdMap.put(item.getName(), item);
+	}
+       	System.out.println(stdMap);
+       
+       	
+      
 		
-		// 성 or 이름 입력 
-		
-		
-		String inName = "";
-		// Map For 문
+       	Scanner sc = new Scanner(System.in);
+		System.out.println("이름 또는 성을 입력하세요");
+       	
+      //user가 입력한 값.
+     	String inName = "신";
+     	
+       //찾은 값을 넣을 list.
 		List<Student> 	stuResultList = new ArrayList<Student>();
-		
+		// Map For 문
 		for(String key : stdMap.keySet() ) {
 			if(key.contains(inName)) {
 				stuResultList.add(stdMap.get(key));
 			}
 			
 		}
-		
-		if( stuResultList.size() > 0 ) {
-			// 출력
-		}
+    	System.out.println(stuResultList);
 
-		
-		
-	}
-	
-	public static void main(String[] args) {
-// 		List<Map<String, List>> student = new ArrayList<>(); 
-
-		CSVreader csvReader = new CSVreader();
-		List<List<String>> list = csvReader.readCSV(); // csv파일 읽어오기
-
-		//StudentMap studentMap = new StudentMap();
-		//studentMap.getName(); // 입력받기
-
-		Map<String, List<Student>> stdMap = new HashMap<String, List<Student>> ();
-
-//		for (List<String> sList : list) {
-//
-//			// ex 김태린 95 80 75 50 75 key = 김, value = [김태린, 95, 80, 75, 50, 75]
-//			//stdMap.put(sList.get(0).substring(0, 1), sList); //중복이 불가능 > 나중에 받은값으로 덮어씀
+//		if( stuResultList.size() > 0 ) {
+//			// 출력
 //		}
-	
-		
-	    System.out.println(stdMap);
-	System.out.println(stdMap.get("김"));
 
-//
+		
+
 	}
 
 }
