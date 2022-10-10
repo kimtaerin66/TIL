cafb8673812f507046715b979d43679e2d8ca3bb
Taerin <85108615+kimtaerin66@users.noreply.github.com>
2022-10-11 00:20:26 +0900

    Create StudentJson.java

---
+++ Java/과제/과제3/StudentJson.java
@@ -0,0 +1,34 @@
+package study2;
+
+import java.util.List;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+public class StudentJson {
+	
+	
+	public static void main(String[] args) {
+
+		JSONArray jsonArr = new JSONArray();
+		JSONObject jsonOb = new JSONObject();
+
+		StudentMap stuResult= new StudentMap();
+		 System.out.println(stuResult);
+//		for(int i=1;i<3;i++) {
+//
+//	        JSONObject data = new JSONObject();
+//
+//	        data.put("이름", "인간_"+i);
+//	        data.put("나이",10+i);
+//	        data.put("주소", "서울 중앙로 "+i+"길");
+//	        jsonarr.add(data);
+//
+//	    }
+//
+//	    jsonob.put("사람들", jsonarr);
+//
+//	    System.out.println(jsonob);
+		
+	}
+
+}
