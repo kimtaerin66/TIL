7f3e9990d287aa3c31e7431d7df45f6124ff11c3
Taerin <85108615+kimtaerin66@users.noreply.github.com>
2022-10-11 16:17:19 +0900

    Update StudentJson.java

--- Java/과제/과제3/StudentJson.java
+++ Java/과제/과제3/StudentJson.java
@@ -1,34 +1,46 @@
-package study2;
+package study3;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
 public class StudentJson {
+
+	// 필터링된 map가져와서 json으로 만들기
+//	private  List<Student> stuList;
+	public JSONObject result;
+
 	
-	
-	public static void main(String[] args) {
+
+
+	public static void main(String[] args) throws IOException {
+		//가져오기
+		StudentMap stuList = new StudentMap();
+		List<Student> slist = stuList.userStuList();		
 
 		JSONArray jsonArr = new JSONArray();
 		JSONObject jsonOb = new JSONObject();
 
-		StudentMap stuResult= new StudentMap();
-		 System.out.println(stuResult);
-//		for(int i=1;i<3;i++) {
-//
-//	        JSONObject data = new JSONObject();
-//
-//	        data.put("이름", "인간_"+i);
-//	        data.put("나이",10+i);
-//	        data.put("주소", "서울 중앙로 "+i+"길");
-//	        jsonarr.add(data);
-//
-//	    }
-//
-//	    jsonob.put("사람들", jsonarr);
-//
-//	    System.out.println(jsonob);
-		
+		for (Student stu : slist) {
+			JSONObject data = new JSONObject();
+           
+           
+	       data.put("이름", stu.getName());
+			data.put("국어", stu.getKorean());
+			data.put("수학", stu.getMath());
+			data.put("영어", stu.getEnglish());
+			data.put("국사", stu.getHistory());
+			data.put("평균", stu.getAvg());
+			jsonArr.add(data);
+
+		}
+	       
+		jsonOb.put("StudentList", jsonArr);
+
+	    System.out.println(jsonOb);
+
 	}
 
 }
